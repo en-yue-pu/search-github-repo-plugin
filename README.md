731532a001d8bfffdc8fd30c37bca7a4cd7b294f
yue_pu <yue_pu@en-japan.com>
2023-06-01 18:32:26 +0900

    成功能跑

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # search-github-repo-plugin
+
+github自带的搜索仓库api
+Get
+https://api.github.com/search/repositories?q=react+language:javascript&sort=stars&order=desc
+
+https://api.github.com/search/repositories
+URLQueryItem(name: "q", value: "\(q)"),
+URLQueryItem(name: "sort", value: "desc"),
+URLQueryItem(name: "order", value: "best match"),
+URLQueryItem(name: "page", value: "1"),
+URLQueryItem(name: "per_page", value: "30"),
+
+
+
+pip install -r requirements.txt
+python main.py
+
---
+++ main.py
@@ -0,0 +1,55 @@
+import json
+import requests
+import quart
+import quart_cors
+from quart import jsonify, request
+
+app = quart.Quart(__name__)
+quart_cors.cors(app, allow_origin="https://chat.openai.com") # 只允许chatgpt官方domin的访问
+
+@app.route("/repos/<string:query>", methods=['GET'])
+async def get_repos(query):
+    url = f"https://api.github.com/search/repositories?q={query}+language:javascript&sort=stars&order=desc"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        return quart.Response(response=json.dumps(data), status=200)
+    else:
+        return quart.Response(response=json.dumps({"error": f"请求失败，状态码：{response.status_code}"}), status=400)
+    
+@app.get("/logo.jpg")#响应读取logo的请求
+async def plugin_logo():
+    filename = 'logo.jpg'
+    try:
+        return await quart.send_file(filename, mimetype='image/jpg')
+    except FileNotFoundError:
+        return jsonify({"error": f"文件'{filename}'不存在"}), 404
+
+@app.get("/.well-known/ai-plugin.json")#响应读取manifest文件的请求
+async def plugin_manifest():
+    host = request.headers['Host']
+    filename = "./.well-known/ai-plugin.json"
+    try:
+        with open(filename) as f:
+            text = f.read()
+            return quart.Response(text, mimetype="text/json")
+    except FileNotFoundError:
+        return jsonify({"error": f"文件'{filename}'不存在"}), 404
+
+@app.get("/openapi.yaml")#响应读取openAPI规范文件(仕様書)的请求
+async def openapi_spec():
+    host = request.headers['Host']
+    filename = "openapi.yaml"
+    try:
+        with open(filename) as f:
+            text = f.read()
+            return quart.Response(text, mimetype="text/yaml")
+    except FileNotFoundError:
+        return jsonify({"error": f"文件'{filename}'不存在"}), 404
+
+def main():
+    app.run(debug=True, host="0.0.0.0", port=5003)#启动服务
+
+if __name__ == "__main__":
+    main()
